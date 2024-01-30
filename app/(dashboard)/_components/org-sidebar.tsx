5d7ae31a5e96fb2afd9d24fad0373d97bd591a3d
Thinh <nguyenhungthinh2504@gmail.com>
2024-01-30 16:25:22 +0700

    Dashboard layout

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const OrgSidebar = () => {
+  return (
+    <div className="hidden lg:flex flex-col space-y-6w w-[206px] pl-5 pt-5 bg-red-500">
+      Org sidebar
+    </div>
+  );
+};
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://first-gecko-84.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
